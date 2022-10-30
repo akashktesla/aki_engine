544e37b8a3842ebbe4829036004f694558c46bb2
akashktesla <akashktesla@gmail.com>
2022-10-31 03:11:35 +0530

    :)

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,212 @@
-//imports
+use aki_engine::{tools,platforms,constants::*, debug::ValidationInfo};
+
+use ash::version::EntryV1_0;
+use ash::version::InstanceV1_0;
+use ash::vk;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_void;
+use std::ptr;
+
 use winit::event::{Event, VirtualKeyCode, ElementState, KeyboardInput, WindowEvent};
 use winit::event_loop::{EventLoop, ControlFlow};
+use winit::window::{Window, WindowBuilder};
 
 // Constants
-//title
-const WINDOW_TITLE: &'static str = "00.Base Code";
-//width and height parameters
-const WINDOW_WIDTH: u32 = 800;
-const WINDOW_HEIGHT: u32 = 600;
+const WINDOW_TITLE: &'static str = "02.Validation Layers";
+const VALIDATION: ValidationInfo = ValidationInfo {
+    is_enable: true,
+    required_validation_layers: ["VK_LAYER_KHRONOS_validation"],
+};
 
-//appstructure
-struct VulkanApp;
+/// the callback function used in Debug Utils.
+unsafe extern "system" fn vulkan_debug_utils_callback(
+    message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+    _p_user_data: *mut c_void,
+) -> vk::Bool32 {
+    let severity = match message_severity {
+        vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE => "[Verbose]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::WARNING => "[Warning]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::ERROR => "[Error]",
+        vk::DebugUtilsMessageSeverityFlagsEXT::INFO => "[Info]",
+        _ => "[Unknown]",
+    };
+    let types = match message_type {
+        vk::DebugUtilsMessageTypeFlagsEXT::GENERAL => "[General]",
+        vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE => "[Performance]",
+        vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION => "[Validation]",
+        _ => "[Unknown]",
+    };
+    let message = CStr::from_ptr((*p_callback_data).p_message);
+    println!("[Debug]{}{}{:?}", severity, types, message);
+
+    vk::FALSE
+}
+
+struct VulkanApp {
+
+    _entry: ash::Entry,
+    instance: ash::Instance,
+    debug_utils_loader: ash::extensions::ext::DebugUtils,
+    debug_merssager: vk::DebugUtilsMessengerEXT,
+}
 
 impl VulkanApp {
+    pub fn new() -> VulkanApp {
 
-    //initializing a normal window
-    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
-        //winit window builder function 
-        winit::window::WindowBuilder::new()
+        // init vulkan stuff
+        let entry = ash::Entry::new().unwrap();
+        let instance = VulkanApp::create_instance(&entry);
+        let (debug_utils_loader, debug_merssager) = VulkanApp::setup_debug_utils(&entry, &instance);
+
+        // cleanup(); the 'drop' function will take care of it.
+        VulkanApp {
+
+            _entry: entry,
+            instance,
+            debug_utils_loader,
+            debug_merssager,
+        }
+    }
+
+    fn init_window(event_loop: &EventLoop<()>) -> Window {
+        WindowBuilder::new()
             .with_title(WINDOW_TITLE)
             .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT))
             .build(event_loop)
             .expect("Failed to create window.")
     }
 
-    //runs every frame IG
-    pub fn main_loop(event_loop: EventLoop<()>) {
+    fn create_instance(entry: &ash::Entry) -> ash::Instance {
+        if VALIDATION.is_enable && VulkanApp::check_validation_layer_support(entry) == false {
+            panic!("Validation layers requested, but not available!");
+        }
+
+        let app_name = CString::new(WINDOW_TITLE).unwrap();
+        let engine_name = CString::new("Vulkan Engine").unwrap();
+        let app_info = vk::ApplicationInfo {
+            p_application_name: app_name.as_ptr(),
+            s_type: vk::StructureType::APPLICATION_INFO,
+            p_next: ptr::null(),
+            application_version: APPLICATION_VERSION,
+            p_engine_name: engine_name.as_ptr(),
+            engine_version: ENGINE_VERSION,
+            api_version: API_VERSION, // set api_version to vk_make_version!(2, 0, 92) to test if the p_next field in vk::InstanceCreateInfo works.
+        };
+
+        // This create info used to debug issues in vk::createInstance and vk::destroyInstance.
+        let debug_utils_create_info = populate_debug_messenger_create_info();
+
+        // VK_EXT debug utils has been requested here.
+        let extension_names = platforms::required_extension_names();
+
+        let requred_validation_layer_raw_names: Vec<CString> = VALIDATION
+            .required_validation_layers
+            .iter()
+            .map(|layer_name| CString::new(*layer_name).unwrap())
+            .collect();
+        let enable_layer_names: Vec<*const i8> = requred_validation_layer_raw_names
+            .iter()
+            .map(|layer_name| layer_name.as_ptr())
+            .collect();
+
+        let create_info = vk::InstanceCreateInfo {
+            s_type: vk::StructureType::INSTANCE_CREATE_INFO,
+            p_next: if VALIDATION.is_enable {
+                &debug_utils_create_info as *const vk::DebugUtilsMessengerCreateInfoEXT
+                    as *const c_void
+            } else {
+                ptr::null()
+            },
+            flags: vk::InstanceCreateFlags::empty(),
+            p_application_info: &app_info,
+            pp_enabled_layer_names: if VALIDATION.is_enable {
+                enable_layer_names.as_ptr()
+            } else {
+                ptr::null()
+            },
+            enabled_layer_count: if VALIDATION.is_enable {
+                enable_layer_names.len()
+            } else {
+                0
+            } as u32,
+            pp_enabled_extension_names: extension_names.as_ptr(),
+            enabled_extension_count: extension_names.len() as u32,
+        };
+
+        let instance: ash::Instance = unsafe {
+            entry
+                .create_instance(&create_info, None)
+                .expect("Failed to create Instance!")
+        };
+
+        instance
+    }
+
+    fn check_validation_layer_support(entry: &ash::Entry) -> bool {
+        // if support validation layer, then return true
+
+        let layer_properties = entry
+            .enumerate_instance_layer_properties()
+            .expect("Failed to enumerate Instance Layers Properties!");
+
+        if layer_properties.len() <= 0 {
+            eprintln!("No available layers.");
+            return false;
+        } else {
+            println!("Instance Available Layers: ");
+            for layer in layer_properties.iter() {
+                let layer_name = tools::vk_to_string(&layer.layer_name);
+                println!("\t{}", layer_name);
+            }
+        }
+
+        for required_layer_name in VALIDATION.required_validation_layers.iter() {
+            let mut is_layer_found = false;
+
+            for layer_property in layer_properties.iter() {
+                let test_layer_name = tools::vk_to_string(&layer_property.layer_name);
+                if (*required_layer_name) == test_layer_name {
+                    is_layer_found = true;
+                    break;
+                }
+            }
+
+            if is_layer_found == false {
+                return false;
+            }
+        }
+
+        true
+    }
+
+    fn setup_debug_utils(
+        entry: &ash::Entry,
+        instance: &ash::Instance,
+    ) -> (ash::extensions::ext::DebugUtils, vk::DebugUtilsMessengerEXT) {
+        let debug_utils_loader = ash::extensions::ext::DebugUtils::new(entry, instance);
+
+        if VALIDATION.is_enable == false {
+            (debug_utils_loader, ash::vk::DebugUtilsMessengerEXT::null())
+        } else {
+            let messenger_ci = populate_debug_messenger_create_info();
+
+            let utils_messenger = unsafe {
+                debug_utils_loader
+                    .create_debug_utils_messenger(&messenger_ci, None)
+                    .expect("Debug Utils Callback")
+            };
+
+            (debug_utils_loader, utils_messenger)
+        }
+    }
+
+    fn draw_frame(&mut self) {
+        // Drawing will be here
+    }
+
+    pub fn main_loop(mut self, event_loop: EventLoop<()>, window: Window) {
 
         event_loop.run(move |event, _, control_flow| {
 
@@ -37,12 +218,9 @@ impl VulkanApp {
                         },
                         | WindowEvent::KeyboardInput { input, .. } => {
                             match input {
-                                //inputs are handled here ig
                                 | KeyboardInput { virtual_keycode, state, .. } => {
                                     match (virtual_keycode, state) {
-                                        //escape = exits the program
                                         | (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
-                                            dbg!();
                                             *control_flow = ControlFlow::Exit
                                         },
                                         | _ => {},
@@ -53,6 +231,12 @@ impl VulkanApp {
                         | _ => {},
                     }
                 },
+                | Event::MainEventsCleared => {
+                    window.request_redraw();
+                },
+                | Event::RedrawRequested(_window_id) => {
+                    self.draw_frame();
+                },
                 _ => (),
             }
 
@@ -60,10 +244,40 @@ impl VulkanApp {
     }
 }
 
+fn populate_debug_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
+    vk::DebugUtilsMessengerCreateInfoEXT {
+        s_type: vk::StructureType::DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+        p_next: ptr::null(),
+        flags: vk::DebugUtilsMessengerCreateFlagsEXT::empty(),
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::WARNING |
+            // vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE |
+            // vk::DebugUtilsMessageSeverityFlagsEXT::INFO |
+            vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+        message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+            | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE
+            | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION,
+        pfn_user_callback: Some(vulkan_debug_utils_callback),
+        p_user_data: ptr::null_mut(),
+    }
+}
+
+impl Drop for VulkanApp {
+    fn drop(&mut self) {
+        unsafe {
+            if VALIDATION.is_enable {
+                self.debug_utils_loader
+                    .destroy_debug_utils_messenger(self.debug_merssager, None);
+            }
+            self.instance.destroy_instance(None);
+        }
+    }
+}
+
 fn main() {
 
     let event_loop = EventLoop::new();
-    let _window = VulkanApp::init_window(&event_loop);
+    let window = VulkanApp::init_window(&event_loop);
 
-    VulkanApp::main_loop(event_loop);
+    let vulkan_app = VulkanApp::new();
+    vulkan_app.main_loop(event_loop, window);
 }
